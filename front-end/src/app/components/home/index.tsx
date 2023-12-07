a6d87bc5ea809aa3278d57a660255fe193eb33ef
Justin <justinkwanchan@gmail.com>
2023-12-07 17:44:39 -0500

    Remove unused sections

--- front-end/src/app/components/home/index.tsx
+++ front-end/src/app/components/home/index.tsx
@@ -1,5 +1,4 @@
 import PreviewSection from './preview-section';
-import DanceSchoolSection from './dance-school-section';
 import PreviousFlyersSection from './previous-flyers-section';
 import PageTitleSection from '../page-title-section';
 import splotch from 'public/splotch-home.svg';
@@ -11,9 +10,6 @@ export default function Home() {
 
       <div className="flex flex-col w-4/5 mb-12 gap-12">
         <PreviewSection title={'Upcoming Social Dances'} />
-        <PreviewSection title={'Newly-Listed Events'} />
-        <PreviewSection title={'Beginner Classes'} />
-        <DanceSchoolSection />
         <PreviousFlyersSection />
       </div>
     </>
--- front-end/src/app/large-events/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function LargeEvents() {
-  return <div>Large Events</div>;
-}
