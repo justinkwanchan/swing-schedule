2112adcb6643e7f3ac17305b34070421c59c8b4a
Justin <justinkwanchan@gmail.com>
2023-12-07 18:10:45 -0500

    Update carousel scroll svgs

--- front-end/src/app/components/home/preview-section/index.tsx
+++ front-end/src/app/components/home/preview-section/index.tsx
@@ -2,8 +2,11 @@
 
 import { useCallback, useEffect, useState } from 'react';
 import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react';
+import Image from 'next/image';
 
 import DanceEventPreviewCard from '../dance-event-preview-card';
+import carouselScrollBack from 'public/carousel-scroll-back.svg';
+import carouselScrollForward from 'public/carousel-scroll-forward.svg';
 
 import styles from '../Carousel.module.css';
 
@@ -64,28 +67,26 @@ export default function PreviewSection({ title }: Props) {
         <h1 className="font-bold text-xl">{title}</h1>
         <div>
           <button
-            className="w-8 h-8 mr-4 disabled:opacity-30"
             onClick={scrollPrev}
             disabled={prevBtnDisabled}
+            className="w-8 h-8 mr-4 disabled:opacity-30"
           >
-            <svg className="w-3/5 h-3/5" viewBox="0 0 532 532">
-              <path
-                fill="currentColor"
-                d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
-              />
-            </svg>
+            <Image
+              src={carouselScrollBack}
+              alt={'Scroll Back'}
+              className="w-3/5 h-3/5 mx-auto"
+            />
           </button>
           <button
-            className="w-8 h-8 disabled:opacity-30"
             onClick={scrollNext}
             disabled={nextBtnDisabled}
+            className="w-8 h-8 disabled:opacity-30"
           >
-            <svg className="w-3/5 h-3/5" viewBox="0 0 532 532">
-              <path
-                fill="currentColor"
-                d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
-              />
-            </svg>
+            <Image
+              src={carouselScrollForward}
+              alt={'Scroll Forward'}
+              className="w-3/5 h-3/5 mx-auto"
+            />
           </button>
         </div>
       </div>
