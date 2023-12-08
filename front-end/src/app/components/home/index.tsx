d8936356707b4e47d695fc5247112b8c0e6d9b04
Justin <justinkwanchan@gmail.com>
2023-12-07 20:52:57 -0500

    Move HomeTitleSection to own component

--- front-end/src/app/components/home/index.tsx
+++ front-end/src/app/components/home/index.tsx
@@ -1,12 +1,11 @@
+import HomeTitleSection from './home-title-section';
 import PreviewSection from './preview-section';
-import PageTitleSection from '../page-title-section';
-import splotch from 'public/splotch-home.svg';
 import { SOCIAL_DANCES, PREVIOUS_FLYERS } from '@/lib/constants';
 
 export default function Home() {
   return (
     <>
-      <PageTitleSection splotch={splotch} />
+      <HomeTitleSection />
 
       <div className="flex flex-col w-4/5 mb-12 gap-12">
         <PreviewSection title={SOCIAL_DANCES} />
