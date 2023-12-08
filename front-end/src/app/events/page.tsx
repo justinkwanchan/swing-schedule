3d70623d557f5dc8e37b56d54ea9f710956ab182
Justin <justinkwanchan@gmail.com>
2023-12-08 01:00:36 -0500

    Add Upcoming Dances (events) page

--- front-end/src/app/events/page.tsx
+++ front-end/src/app/events/page.tsx
@@ -1,3 +1,39 @@
+import PageTitleSection from '@/app/components/page-title-section';
+import DanceEventPreviewCard from '../components/home/dance-event-preview-card';
+import splotch from 'public/splotch.svg';
+
 export default function Events() {
-  return <div>Events</div>;
+  const tempEvent = {
+    id: 'cats78',
+    image: 'image',
+    datetime: 'FRI OCT 16 @ 7:30PM',
+    title: "Cat's Corner Weekly Dance",
+    organizer: "Cat's Corner",
+    location: 'Polish White Eagle Society',
+    address: '1956 Rue Frontenac, Montréal, QC H2K 2Z1',
+  };
+
+  return (
+    <div className="flex flex-col items-center mb-12">
+      <PageTitleSection splotch={splotch} />
+
+      <div className="flex flex-wrap w-4/5 mb-12 gap-y-12 -ml-8">
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+        <DanceEventPreviewCard event={tempEvent} />
+      </div>
+    </div>
+  );
 }
