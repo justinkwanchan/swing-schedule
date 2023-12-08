91f0b4bc45e058e13a95ef29477ca942fa7d394d
Justin <justinkwanchan@gmail.com>
2023-12-08 00:56:28 -0500

    Improve responsiveness of preview cards

--- front-end/src/app/components/home/dance-event-preview-card/index.tsx
+++ front-end/src/app/components/home/dance-event-preview-card/index.tsx
@@ -8,7 +8,7 @@ export default function DanceEventPreviewCard({ event }: Props) {
   const { id, image, datetime, title, organizer, location, address } = event;
 
   return (
-    <div className="flex flex-col flex-[0_0_33.333333%] pl-8">
+    <div className="flex flex-col flex-[0_0_100%] sm:flex-[0_0_50%] lg:flex-[0_0_33.333333%] 2xl:flex-[0_0_25%] pl-8">
       <div className="bg-gray-100 w-full h-48 rounded-lg mb-2"></div>
       <h3 className="text-red-500">{datetime}</h3>
       <h1 className="font-bold text-lg mb-2">{title}</h1>
