29824fe823e697e345a8c793044f727f1b45a553
Justin <justinkwanchan@gmail.com>
2023-12-15 15:37:21 -0500

    Add mobile layout for Contribution page

--- front-end/src/app/contribute/page.tsx
+++ front-end/src/app/contribute/page.tsx
@@ -11,16 +11,19 @@ export default function Contribute() {
 
       <div className="w-11/12">
         <div className="flex gap-8 mb-16">
-          <div className="flex flex-col gap-12 w-1/2">
-            <div>
-              <h2 className="text-2xl font-medium mb-4">
-                Step 1: Make an account
-              </h2>
+          <div className="flex flex-col gap-12 md:w-1/2">
+            <div className="flex flex-col gap-4">
+              <h2 className="text-2xl font-medium">Step 1: Make an account</h2>
               <p className="text-sub-text-grey">
                 Are you a swing dance organization looking to promote and
                 include others in your scene? Create an account to recieve event
                 creation access.
               </p>
+              <Image
+                src={danceDude}
+                alt={'Dancing Dude'}
+                className="self-center md:hidden w-4/5"
+              />
             </div>
             <div>
               <h2 className="text-2xl font-medium mb-4">
@@ -30,7 +33,7 @@ export default function Contribute() {
                 Not just any event... a swing dance event. See below for
                 requirements!
               </p>
-              <ul className="flex flex-wrap justify-between gap-y-8">
+              <ul className="flex flex-col md:flex-row flex-wrap justify-between gap-y-8">
                 <li className="w-1/2">Ipsum</li>
                 <li className="w-1/2">Ipsum</li>
                 <li className="w-1/2">Ipsum</li>
@@ -50,7 +53,11 @@ export default function Contribute() {
             </div>
           </div>
 
-          <Image src={danceDude} alt={'Dancing Dude'} className="w-5/12" />
+          <Image
+            src={danceDude}
+            alt={'Dancing Dude'}
+            className="hidden md:block w-5/12"
+          />
         </div>
 
         <div className="flex items-center">
@@ -58,7 +65,7 @@ export default function Contribute() {
             Ready to create your event?
           </h2>
           <Link href="">
-            <button className="font-bold bg-[#BDFFF3] px-8 py-4 rounded-2xl shadow-[4px_6px_8px_0_rgba(0,0,0,0.25)]">
+            <button className="font-bold bg-[#BDFFF3] w-max px-8 py-4 rounded-2xl shadow-[4px_6px_8px_0_rgba(0,0,0,0.25)]">
               Create Event
             </button>
           </Link>
