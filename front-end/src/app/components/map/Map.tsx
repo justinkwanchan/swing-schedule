d8b014b8402750affee642f3e6c2dc079c3b60d1
Justin <justinkwanchan@gmail.com>
2023-12-16 04:11:06 -0500

    Add map marker

--- front-end/src/app/components/map/Map.tsx
+++ front-end/src/app/components/map/Map.tsx
@@ -1,5 +1,7 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 
 type Props = {
   center: [number, number];
@@ -17,6 +19,7 @@ export default function Map({ center }: Props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={center} interactive={false} />
     </MapContainer>
   );
 }
