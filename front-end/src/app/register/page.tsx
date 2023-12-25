f8492a5fadf2b8d8eddeb93f8553cdda1008d1f7
Justin <justinkwanchan@gmail.com>
2023-12-24 21:19:32 -0500

    Add initial layout to register page

--- front-end/src/app/register/page.tsx
+++ front-end/src/app/register/page.tsx
@@ -1,3 +1,24 @@
+import Image from 'next/image';
+import alyssa from 'public/Alyssa.png';
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="flex h-[calc(100svh-238px)] w-full bg-dark-grey">
+      <div className="flex gap-8 bg-white w-11/12 h-[95%] rounded-xl m-auto p-6">
+        <div className="hidden md:flex w-1/2">
+          <Image
+            src={alyssa}
+            alt={'Alyssa dancing'}
+            className="rounded-xl w-auto h-auto max-h-full m-auto"
+          />
+        </div>
+
+        <div className="flex flex-col w-full md:w-1/2">
+          <h1 className="text-4xl font-medium self-center">
+            Create your account
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
