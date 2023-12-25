75e3e5b47179b2b4db66592039a062d8138f8dfa
Justin <justinkwanchan@gmail.com>
2023-12-25 12:14:49 -0500

    Add more content to Register page

--- front-end/src/app/register/page.tsx
+++ front-end/src/app/register/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import alyssa from 'public/Alyssa.png';
 
 export default function Register() {
   return (
     <div className="flex h-[calc(100svh-238px)] w-full bg-dark-grey">
       <div className="flex gap-8 bg-white w-11/12 h-[95%] rounded-xl m-auto p-6">
-        <div className="hidden lg:flex w-1/2">
+        <div className="hidden md:flex w-1/2">
           <Image
             src={alyssa}
             alt={'Alyssa dancing'}
@@ -13,11 +14,12 @@ export default function Register() {
           />
         </div>
 
-        <div className="w-full lg:w-1/2">
-          <div className="flex flex-col w-full lg:w-5/6 m-auto">
+        <div className="w-full md:w-1/2">
+          <div className="flex flex-col w-full md:w-5/6 m-auto">
             <h1 className="text-3xl font-medium self-center">
               Create your account
             </h1>
+
             <form>
               <div className="mt-8 grid grid-cols-2 gap-x-6 gap-y-2">
                 <div className="col-span-2">
@@ -110,12 +112,33 @@ export default function Register() {
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-transparent"
                     />
                   </div>
-                  <label htmlFor="remember" className="text-xs text-gray-900">
+                  <label
+                    htmlFor="remember"
+                    className="text-xs text-sub-text-grey"
+                  >
                     Remember me for 30 days
                   </label>
                 </div>
               </div>
             </form>
+
+            <div className="relative flex w-full h-8 mt-4">
+              <div className="w-full h-[1px] bg-gradient-to-r from-white via-light-grey to-white m-auto"></div>
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-1.5 bg-white border rounded-full text-xs text-sub-text-grey">
+                OR
+              </div>
+            </div>
+
+            <button className="w-full py-3 border rounded-2xl mt-6 text-sm text-sub-text-grey">
+              Continue with Google
+            </button>
+
+            <div className="flex gap-1 self-center text-xs mt-12">
+              <p className="text-sub-text-grey">Already have an account?</p>
+              <Link href="/login" className="text-[#FF5678]">
+                Login
+              </Link>
+            </div>
           </div>
         </div>
       </div>
