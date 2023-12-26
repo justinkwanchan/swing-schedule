ce86396ef2647a938f5a1f18e58c6c289a5aafc8
Justin <justinkwanchan@gmail.com>
2023-12-26 12:54:38 -0500

    Update subtext on HomeTitleSection

--- front-end/src/app/components/home/home-title-section/index.tsx
+++ front-end/src/app/components/home/home-title-section/index.tsx
@@ -11,7 +11,7 @@ export default function HomeTitleSection() {
           alt={'This Week in Montreal'}
           className="-mt-16 w-[75%] mx-auto md:absolute md:z-10 md:top-8 md:left-8 md:translate-x-0 md:mt-0 md:w-[100%]"
         />
-        <p className="my-4 mx-14 md:mt-[42vw] xl:mt-[40vw] xl:text-lg 2xl:mt-[34vw]">
+        <p className="my-4 mx-14 text-dark-grey tracking-widest md:mt-[42vw] xl:mt-[40vw] xl:text-lg 2xl:mt-[34vw]">
           Your go-to guide for swing dance socials in Montreal! Explore upcoming
           events and connect with the local swing dance community.
         </p>
