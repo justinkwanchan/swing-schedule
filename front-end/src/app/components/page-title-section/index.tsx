af26cce98587b6287a05bcb3c8f685ba28d029d7
Justin <justinkwanchan@gmail.com>
2024-01-03 05:57:46 -0500

    Improve font size in PageTitleSection

--- front-end/src/app/components/page-title-section/index.tsx
+++ front-end/src/app/components/page-title-section/index.tsx
@@ -8,20 +8,20 @@ type Props = {
 
 export default function PageTitleSection({ title, description }: Props) {
   return (
-    <section className="flex flex-col md:flex-row h-[calc(80svh-62px)] w-full mb-12">
-      <div className="flex relative bg-light-grey h-[70vw] md:h-auto md:w-3/5">
+    <section className="flex flex-col-reverse md:flex-row w-full mb-12">
+      <div className="flex relative bg-light-grey h-[70vw] max-h-[40svh] md:h-auto md:max-h-[65svh] md:flex-[0_0_60%] 2xl:flex-[0_0_50%]">
         <Image
           src={splotch}
           alt={'Splotchy Frame'}
           className="m-auto w-[75%] md:w-4/5 h-[90%]"
         />
-        <h1 className="text-6xl text-center font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[33%] w-4/5 sm:text-7xl md:text-4xl lg:text-6xl xl:text-8xl">
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[33%] w-4/5 text-6xl text-center font-bold sm:text-7xl md:text-6xl lg:text-8xl xl:text-9xl">
           {title}
         </h1>
       </div>
 
-      <div className="flex bg-dark-grey md:h-auto md:grow">
-        <h2 className="text-white tracking-widest m-auto py-6 px-8 md:p-32">
+      <div className="flex bg-dark-grey md:grow md:h-auto">
+        <h2 className="text-white tracking-widest max-w-md m-auto py-6 px-8 md:py-12 lg:text-xl lg:max-w-md xl:text-2xl xl:max-w-lg">
           {description}
         </h2>
       </div>
