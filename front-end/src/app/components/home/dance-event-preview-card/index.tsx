a2d40cba288fd49a915237babb6dd7c56033b1ed
Justin <justinkwanchan@gmail.com>
2024-01-03 01:21:49 -0500

    Add comments

--- front-end/src/app/components/home/dance-event-preview-card/index.tsx
+++ front-end/src/app/components/home/dance-event-preview-card/index.tsx
@@ -8,10 +8,12 @@ type Props = {
 export default function DanceEventPreviewCard({ event }: Props) {
   return (
     <div className="flex-[0_0_170px] pl-4 md:pl-8 md:flex-[0_0_50%] lg:flex-[0_0_33.333333%] 2xl:flex-[0_0_25%]">
+      {/* Mobile view */}
       <Link href={`events/${event.id}`} className="md:hidden">
         <Card event={event} />
       </Link>
 
+      {/* Desktop view */}
       <div className="hidden md:block">
         <Card event={event} />
       </div>
